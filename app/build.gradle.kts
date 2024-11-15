371ad07f84148aed20d39e44c310fb3609705e41
slato05 <slato2001@gmail.com>
2024-11-14 18:34:26 -0600

    Working Version (w/ database and coroutines and disabled schema export)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,8 @@ android {
     viewBinding {
         enable = true
     }
+
+
 }
 
 dependencies {
--- app/src/main/java/com/example/itc362_hw7_ex1a/database/CrimeDatabase.kt
+++ app/src/main/java/com/example/itc362_hw7_ex1a/database/CrimeDatabase.kt
@@ -7,7 +7,7 @@ import com.example.itc362_hw7_ex1a.Crime
 import androidx.room.Database as Database1
 
 
-@Database1(entities = [Crime:: class], version=1)
+@Database1(entities = [Crime:: class], version=1, exportSchema = false)
 @TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase: RoomDatabase() {
     abstract fun crimeDAO(): CrimeDao
